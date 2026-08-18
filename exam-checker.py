1bc09d1598e8fe672713530a54a0792fbd935cbc
shacbi <shaxyardls24@gmail.com>
2026-08-18 22:50:46 +1000

    casarada jamacadda

--- exam-checker.py
+++ exam-checker.py
@@ -2,14 +2,14 @@
 
 print("=== UNIVERSITY RESULT CHECKER ===")
 
-marks = float(input("Enter your exam marks (0 - 100): "))
+marks = float(input("gali your exam marks (0 - 100): "))
 
 if marks >= 50:
     result = "PASSED"
-    message = "Congratulations! You passed the exam."
+    message = "hanbalyo! You passed the exam."
 else:
     result = "FAILED"
-    message = "Sorry, you need to retake the exam."
+    message = "waan ka xuma hay, you need to retake the exam."
 
 print(f"\nFinal Result: {result}")
 print(f"Message: {message}")
